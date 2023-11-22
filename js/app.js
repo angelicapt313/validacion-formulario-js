6a9863d5e2faa8128b8faee392af22a2a2f8d7e9
Ag <puchetatome.angelica@gmail.com>
2023-11-22 14:10:45 -0600

    I added a function to clean up the validations error.

--- js/app.js
+++ js/app.js
@@ -9,27 +9,39 @@ textArea.addEventListener('blur', validations)
 
 
 function validations(e){
+
+
     if(e.target.value.trim() === ''){
         showAlert(`The field ${e.target.id} is required.`, e.target.parentElement);
+        return
+    }else{
+        let success = e.target;
+        success.classList.add('success');
     }
+
+    cleanFields(e.target.parentElement)
+   
 }
 
 function showAlert(string, referenceMessage){
 
-    const deleteMultipleAlerts = referenceMessage.querySelector('.nombreInput--error', '.borderError--input')
-
-    if(deleteMultipleAlerts){
-        deleteMultipleAlerts.remove();
-    }
-
+    cleanFields(referenceMessage);
   
     let messageError = document.createElement('P');
     messageError.textContent = string;
     messageError.classList.add('nombreInput--error');
-    let borderInputError = referenceMessage.lastElementChild;
+
+    let borderInputError = referenceMessage.querySelector('.form-control');
     borderInputError.classList.add('borderError--input');
   
     referenceMessage.appendChild(messageError);
 
+}
+
+function cleanFields(reference){
+    let deleteMultipleAlerts = reference.querySelector('.nombreInput--error', '.borderError--input');
+    if(deleteMultipleAlerts){
+        deleteMultipleAlerts.remove();
+    }
 }
 
