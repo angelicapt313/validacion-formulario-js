0988185ef0720794a905024d393e560d7df493ea
Ag <puchetatome.angelica@gmail.com>
2023-12-12 11:21:09 -0600

    uncomment the JS code and fixed the form tag.

--- js/app.js
+++ js/app.js
@@ -1,123 +1,124 @@
-// const nombre = document.querySelector('.nombreInput');
-// const email = document.querySelector('.emailInput');
-// const textArea = document.querySelector('.textArea');
-// const btnSend = document.querySelector('#formulario button[type="submit"]')
-// const btnReset = document.querySelector('#formulario button[type="reset"]');
-// const formulario = document.querySelector('#formulario');
-// const spinner = document.querySelector('#spinner');
+const nombre = document.querySelector('.nombreInput');
+const email = document.querySelector('.emailInput');
+const textArea = document.querySelector('.textArea');
+const btnSend = document.querySelector('#formulario button[type="submit"]')
+const btnReset = document.querySelector('#formulario button[type="reset"]');
+const formulario = document.querySelector('#formulario');
+const spinner = document.querySelector('#spinner');
 
-// nombre.addEventListener('input', validations);
-// email.addEventListener('input', validations);
-// textArea.addEventListener('input', validations)
-
-
-// const saveValuesForm = {
-//     name: '',
-//     email: '',
-//     comments: ''
-// };
-
-// btnReset.addEventListener('click', function(){
-//     resetForm();
-// });
-
-// formulario.addEventListener('submit', sendForm);
-
-// function validations(e) {
-
-//     if (e.target.value.trim() === '') {
-//         showAlert(`The field ${e.target.id} is required.`, e.target.parentElement);
-//         saveValuesForm[e.target.id] = '';
-//         validateFieldsForm();
-//         return
-//     } 
+nombre.addEventListener('input', validations);
+email.addEventListener('input', validations);
+textArea.addEventListener('input', validations)
 
 
-//     if (e.target.id === 'email' && !validateEmail(e.target.value)) {
-//         showAlert('Email incorrect', e.target.parentElement); 
-//         saveValuesForm[e.target.id] = '';
-//         validateFieldsForm();
-//         return
-//     }
+const saveValuesForm = {
+    name: '',
+    email: '',
+    comments: ''
+};
 
-//     cleanFields(e.target.parentElement);
+btnReset.addEventListener('click', function(){
+    resetForm();
+});
+
+formulario.addEventListener('submit', sendForm);
+
+function validations(e) {
+
+    if (e.target.value.trim() === '') {
+        showAlert(`The field ${e.target.id} is required.`, e.target.parentElement);
+        saveValuesForm[e.target.id] = '';
+        validateFieldsForm();
+        return
+    } 
+
+
+    if (e.target.id === 'email' && !validateEmail(e.target.value)) {
+        showAlert('Email incorrect', e.target.parentElement); 
+        saveValuesForm[e.target.id] = '';
+        validateFieldsForm();
+        return
+    }
+
+    cleanFields(e.target.parentElement);
     
-//     saveValuesForm[e.target.id] = e.target.value.trim().toLowerCase();
+    saveValuesForm[e.target.id] = e.target.value.trim().toLowerCase();
 
-//     validateFieldsForm();
-// }
+    validateFieldsForm();
+}
 
-// function showAlert(string, referenceMessage) {
+function showAlert(string, referenceMessage) {
 
-//     cleanFields(referenceMessage);
+    cleanFields(referenceMessage);
 
-//     let messageError = document.createElement('P');
-//     messageError.textContent = string;
-//     messageError.classList.add('nombreInput--error');
+    let messageError = document.createElement('P');
+    messageError.textContent = string;
+    messageError.classList.add('nombreInput--error');
 
-//     let inputError = referenceMessage.querySelector('.form-control');
-//     inputError.classList.add('borderError--input')
+    let inputError = referenceMessage.querySelector('.form-control');
+    inputError.classList.add('borderError--input')
 
-//     referenceMessage.appendChild(messageError);
+    referenceMessage.appendChild(messageError);
 
-// }
+}
 
-// function cleanFields(reference) {
-//     let deleteMultipleAlerts = reference.querySelector('.nombreInput--error');
+function cleanFields(reference) {
+    let deleteMultipleAlerts = reference.querySelector('.nombreInput--error');
 
-//     if (deleteMultipleAlerts) {
-//         deleteMultipleAlerts.remove();
-//     }
-// }
+    if (deleteMultipleAlerts) {
+        deleteMultipleAlerts.remove();
+    }
+}
 
-// function validateEmail(email) {
-//     let regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-//     let result = regex.test(email);
-//     return result;
-// }
+function validateEmail(email) {
+    let regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    let result = regex.test(email);
+    return result;
+}
 
-// function validateFieldsForm(){
-//     if(Object.values(saveValuesForm).includes('')){
-//         btnSend.disabled = true;
-//         return
-//     }
-//         btnSend.disabled = false;
+function validateFieldsForm(){
+    if(Object.values(saveValuesForm).includes('')){
+        btnSend.disabled = true;
+        return
+    }
+        btnSend.disabled = false;
     
-// }
+}
 
-// function resetForm(){
-//     saveValuesForm.name = '';
-//     saveValuesForm.email = '';
-//     saveValuesForm.comments = '';
+function resetForm(){
+    saveValuesForm.name = '';
+    saveValuesForm.email = '';
+    saveValuesForm.comments = '';
 
-//     formulario.reset();
+    formulario.reset();
 
-//     validateFieldsForm();
-// }
+    validateFieldsForm();
+}
 
-// function sendForm(e){
-//     e.preventDefault();
+function sendForm(e){
+    e.preventDefault();
 
-//     spinner.classList.add('d-flex');
-//     spinner.classList.remove('visually-hidden');
+    
+    spinner.classList.add('d-flex');
+    spinner.classList.remove('visually-hidden');
 
-//     setTimeout(()=>{
-//         spinner.classList.remove('d-flex');
-//         spinner.classList.add('visually-hidden'); 
+    setTimeout(()=>{
+        spinner.classList.remove('d-flex');
+        spinner.classList.add('visually-hidden'); 
 
-//         resetForm();
+        resetForm();
 
-//         const msgSuccess = document.createElement('P');
-//         msgSuccess.textContent = 'Message sent succesfully!';
-//         msgSuccess.classList.add('fs-3', 'text-success', 'mt-2', 'text-center', 'fw-bold');
-//         formulario.appendChild(msgSuccess);
+        const msgSuccess = document.createElement('P');
+        msgSuccess.textContent = 'Message sent succesfully!';
+        msgSuccess.classList.add('fs-3', 'text-success', 'mt-2', 'text-center', 'fw-bold');
+        formulario.appendChild(msgSuccess);
 
-//         setTimeout(()=>{
-//             msgSuccess.remove();
-//         }, 4000)
+        setTimeout(()=>{
+            msgSuccess.remove();
+        }, 4000)
 
-//     }, 3000)
+    }, 3000)
 
    
-// }
+}
 
